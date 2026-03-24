9418f5b931bf05a9831610f18ca00796419d2d15
22-2 <17890m@protonmail.ch>
2026-03-24 16:38:40 +0900

    chore: remove CommonJS output and update TypeScript module settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm"],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   minify: false,
-  target: "node16",
+  target: "esnext",
   outDir: "dist",
 });
