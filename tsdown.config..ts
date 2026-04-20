5ab1acf4a1f8a89c7e1b5e1cb6c97d0230ccfac7
22-2 <17890m@protonmail.ch>
2026-04-21 01:52:07 +0900

    chore: replace tsup with tsdown

--- tsdown.config..ts
+++ tsdown.config..ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
+// @ts-expect-error
+
 import { builtinModules } from "node:module";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
   dts: true,
-  splitting: false,
   sourcemap: true,
   clean: true,
   minify: false,
